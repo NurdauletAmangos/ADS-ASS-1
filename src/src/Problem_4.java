95eb7d78f026a1ca207edee72de06751a3c08d5f
Nurdauletamangos <nurdauletamangosvps@gmail.com>
2025-07-13 23:14:14 +0500

    Problem 4 solution

---
+++ src/src/Problem_4.java
@@ -0,0 +1,7 @@
+public class Problem_4 {
+    public static int factorial(int n) {
+        if (n < 0) throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+        if (n == 0 || n == 1) return 1;
+        return n * factorial(n - 1);
+    }
+}
